a884cfa693ebf515f0214b2d25702801df592b7a
Gerald <Fareltripp@gmail.com>
2019-08-02 10:58:39 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//Get Input Element
+
+let filterInput = document.getElementById("filterInput");
+
+//Add Event Listener
+
+filterInput.addEventListener("keyup", filterNames);
+
+function filterNames() {
+  console.log(1);
+}
