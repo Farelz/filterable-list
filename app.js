fbf3bd68b21369af1c0e9ab0d09d05f59264507a
Gerald <Fareltripp@gmail.com>
2019-10-11 01:13:17 +0100

    Editing the App file

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ filterInput.addEventListener("keyup", filterNames);
 
 function filterNames() {
   //Get Value Of Input
-  let filterValue = document.getElementById('filterInput').value.toUpperCase();
+  let filterValue = document.getElementById("filterInput").value.toUpperCase();
   console.log(filterValue);
+
+  //Get names ul
+  let ul = document.getElementById("names");
+
+  //Get lis from ul
+  let li = ul.querySelectorAll("li.collection-item");
+
+  //Loop through collection-item lis
+  for (let i = 0; i < li.length; i++) {
+    let a = li[i].getElementsByTagName("a")[0];
+
+    //if matched
+    if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 }
